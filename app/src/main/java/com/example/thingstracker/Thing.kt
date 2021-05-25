bfd30898128727fbd400df7de85757a8450cad13
Dawid Czyżewski <dawidek12.04@wp.pl>
2021-05-25 12:54:31 +0200

    feat: add Thing class fields (name,place,date,time)

--- app/src/main/java/com/example/thingstracker/Thing.kt
+++ app/src/main/java/com/example/thingstracker/Thing.kt
@@ -1,5 +1,8 @@
 package com.example.thingstracker
 
 class Thing {
-
+    private lateinit var name: String
+    private lateinit var place: String
+    private lateinit var date: String
+    private lateinit var time: String
 }
