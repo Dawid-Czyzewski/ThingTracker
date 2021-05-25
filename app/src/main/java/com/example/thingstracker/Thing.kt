98bc33e9e2637f13b5b3b08e976186f83ea42415
Dawid Czyżewski <dawidek12.04@wp.pl>
2021-05-25 14:56:50 +0200

    feat: add get and set functions

--- app/src/main/java/com/example/thingstracker/Thing.kt
+++ app/src/main/java/com/example/thingstracker/Thing.kt
@@ -5,4 +5,32 @@ class Thing {
     private lateinit var place: String
     private lateinit var date: String
     private lateinit var time: String
+
+    fun  getName(): String {
+        return name
+    }
+    fun  setName(_name: String) {
+        name = _name
+    }
+
+    fun  getPlace(): String {
+        return place
+    }
+    fun  setPlace(_place: String) {
+        place = _place
+    }
+
+    fun  getDate(): String {
+        return date
+    }
+    fun  setDate(_date: String) {
+        date = _date
+    }
+
+    fun  getTime(): String {
+        return time
+    }
+    fun  setTime(_time: String) {
+        time = _time
+    }
 }
