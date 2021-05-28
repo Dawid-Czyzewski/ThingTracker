df5945badd08e0c9df9e806fd58895e9fa7621e9
Dawid Czyżewski <dawidek12.04@wp.pl>
2021-05-28 11:24:32 +0200

    feat: add goToNewActivity function

--- app/src/main/java/com/example/thingstracker/Activities/MainActivity.kt
+++ app/src/main/java/com/example/thingstracker/Activities/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.thingstracker.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Display
 import android.view.View
 import com.example.thingstracker.R
 import kotlin.system.exitProcess
@@ -14,5 +16,10 @@ class MainActivity : AppCompatActivity() {
 
     fun quit(view: View){
         exitProcess(-1)
+    }
+
+    fun goToAddNewThinkActivity(view:View){
+        val intent = Intent(this,NewThinkActivity::class.java)
+        startActivity(intent)
     }
 }
