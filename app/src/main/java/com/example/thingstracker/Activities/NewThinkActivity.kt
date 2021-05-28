2476867916ffd707558cc9da53b494653fe1231f
Dawid Czyżewski <dawidek12.04@wp.pl>
2021-05-28 11:11:51 +0200

    feat: add new activity

---
+++ app/src/main/java/com/example/thingstracker/Activities/NewThinkActivity.kt
@@ -0,0 +1,12 @@
+package com.example.thingstracker.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.thingstracker.R
+
+class NewThinkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_think)
+    }
+}
