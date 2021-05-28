7fe0975d2b0157e209b89f568978316b8e3e8dad
Dawid Czyżewski <dawidek12.04@wp.pl>
2021-05-28 11:33:41 +0200

    feat: add backToMenu function

--- app/src/main/java/com/example/thingstracker/Activities/NewThinkActivity.kt
+++ app/src/main/java/com/example/thingstracker/Activities/NewThinkActivity.kt
@@ -1,12 +1,20 @@
 package com.example.thingstracker.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.thingstracker.R
 
 class NewThinkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_think)
+    }
+
+
+    fun backToMenu(view: View){
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 }
